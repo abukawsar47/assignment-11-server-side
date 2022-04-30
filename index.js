4befc4325cf182f05e592a244326c95fd212042b
abukawsar47 <abukawsar47ak@gmail.com>
2022-04-30 21:39:45 +0600

    project initialization

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lcmpo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const carCollection = client.db('carDealer').collection('car')
+
+        app.get('/car', async (req, res) => {
+            const query = {};
+            const cursor = carCollection.find(query);
+            const cars = await cursor.toArray();
+            res.send(cars);
+        })
+
+
+        app.get('/car/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            console.log(query);
+            const car = await carCollection.findOne(query);
+            res.send(car);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
+
+
+
+
+//to check is it working
+app.get('/', (req, res) => {
+    res.send('Running Server');
+});
+
+//listen
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
