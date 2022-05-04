d135949b1b199f8b68a05cace8f87d8381bef717
abukawsar47 <abukawsar47ak@gmail.com>
2022-05-04 21:04:00 +0600

    jwt api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -33,6 +34,16 @@ async function run() {
             res.send(car);
         })
 
+        // =================
+        /*      app.get('/car', async (req, res) => {
+                 const query = {};
+                 const cursor = carCollection.find(query);
+                 const supplierInfo = await cursor.toArray();
+                 res.send(supplierInfo);
+             }); */
+        // ==================
+
+
         //POST
         app.post('/car', async (req, res) => {
             const newCar = req.body;
@@ -52,17 +63,37 @@ async function run() {
         app.put('/car/:id', async (req, res) => {
             const id = req.params.id;
             const updateCar = req.body;
-            const filter = { _id: ObjectId(id) };
+            const query = { _id: ObjectId(id) };
             const options = { upsert: true };
             const updateDoc = {
                 $set: {
                     quantity: updateCar.quantity
                 }
             };
-            const result = await carCollection.updateOne(filter, updateDoc, options);
+            const result = await carCollection.updateOne(query, updateDoc, options);
             res.send(result);
         })
-
+        // verifyJWT
+        app.get('/car', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = carCollection.find(query);
+                const cars = await cursor.toArray();
+                res.send(cars);
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden Access' })
+            }
+        })
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
 
     }
     finally {
