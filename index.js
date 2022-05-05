f64144e971dacdb5bd5729f97794d45c975852fa
abukawsar47 <abukawsar47ak@gmail.com>
2022-05-05 11:02:42 +0600

    login varify

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lcmpo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -73,20 +91,20 @@ async function run() {
             const result = await carCollection.updateOne(query, updateDoc, options);
             res.send(result);
         })
-        // verifyJWT
         app.get('/car', verifyJWT, async (req, res) => {
             const decodedEmail = req.decoded.email;
             const email = req.query.email;
             if (email === decodedEmail) {
                 const query = { email: email };
                 const cursor = carCollection.find(query);
-                const cars = await cursor.toArray();
-                res.send(cars);
+                const result = await cursor.toArray();
+                res.send(result);
             }
             else {
-                return res.status(403).send({ message: 'Forbidden Access' })
+                res.status(403).send({ message: 'forbidden access' })
             }
         })
+        // AUTH
         app.post('/login', async (req, res) => {
             const user = req.body;
             const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
